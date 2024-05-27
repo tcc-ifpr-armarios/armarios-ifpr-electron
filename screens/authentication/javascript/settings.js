9233638a69856617f30fe1f873f7e72d343ee688
Allan Fernando Oliveira de Andrade <allan-sjp@hotmail.com>
2024-05-27 15:37:12 -0300

    Initial commit - basic application

---
+++ screens/authentication/javascript/settings.js
@@ -0,0 +1,6 @@
+var CLIENT_ID = '';
+var REDIRECT_URI = 'http://localhost:8080';
+var SUAP_URL = 'https://suap.ifpr.edu.br';
+var SCOPE = 'identificacao email documentos_pessoais';
+//s7z5Too8oq1bbneTpCzQFvef1S2yitdBSCbXhrmZAanGWzVLyflHfLxFykr0A8QZCqzTP4UkD7n5ghqjRcpc7V1VL5zDonxLwfB3WvTzzOW1rchJORLRJ3uDvXtB3kuY
+//client secret, nõ sei pra serve
