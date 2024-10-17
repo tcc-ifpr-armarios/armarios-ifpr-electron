252a39091203ad91aa6cbe512743b59ca73ad6df
Allan Fernando Oliveira de Andrade <allan-sjp@hotmail.com>
2024-10-17 19:23:11 -0300

    feat: login com cronos funcional

--- public/home/javascript/index.js
+++ public/home/javascript/index.js
@@ -1,10 +1,15 @@
 // script.js
 document.getElementById('register').addEventListener('click', function () {
-    window.location.href = '../register/index.html'; // Altere para o URL desejado
+    window.location.href = '../register/index.html';
     console.log("Redirecionando para a tela de cadastro");
 });
 
 document.getElementById('administer').addEventListener('click', function () {
-    window.location.href = '../authentication/servidor/index.html'; // Altere para o URL desejado
+    window.location.href = '../authentication/servidor/index.html';
+    console.log("Redirecionando para a tela de login");
+});
+
+document.getElementById('login').addEventListener('click', function () {
+    window.location.href = '../authentication/estudante/index.html';
     console.log("Redirecionando para a tela de login");
 });
